b8bf89ef167e6b4d336051390c126ea68aebd480
Timo Reimann <ttr314@googlemail.com>
2020-08-08 07:26:33 +0200

    Remove unnecessary fields

--- race.go
+++ race.go
@@ -6,7 +6,6 @@ import (
 )
 
 type Tx struct {
-	val  int
 	ctx  context.Context
 	done bool
 }
@@ -25,16 +24,13 @@ type Dependency interface {
 
 type server struct {
 	dep Dependency
-	res int
 }
 
 func (d *server) Unsubscribe(ctx context.Context) error {
 	txCtx, cancel := context.WithCancel(ctx)
 	defer cancel()
-	tx := &Tx{val: 22, ctx: txCtx}
+	tx := &Tx{ctx: txCtx}
 	tx.awaitDone()
-
-	d.res = tx.val
 
 	if err := d.dep.End(ctx, tx); err != nil {
 		return fmt.Errorf("End failed: %s", err)
